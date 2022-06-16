3d031b6259d61ee9c32ef0ad99d1c688d7024ca5
kasmello <karmel.gandahusada@gmail.com>
2022-06-16 17:40:44 +0800

    general updates

--- pricetracker/pricetracker/settings.py
+++ pricetracker/pricetracker/settings.py
@@ -38,9 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest-framework',
+    'rest_framework',
     'api',
-    'frontend'
+    'frontend',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
