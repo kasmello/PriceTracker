7965cf7557860baeb1ef1ddbf43ef76b206bd628
kasmello <karmel.gandahusada@gmail.com>
2022-06-22 20:37:46 +0800

    added nav bar for fuel

--- pricetracker/frontend/frontend/src/components/misc_pages.js
+++ pricetracker/frontend/frontend/src/components/misc_pages.js
@@ -8,4 +8,16 @@ function PageNotFound() {
     )
 };
 
-export default PageNotFound
+function About() {
+    return (
+        <div>
+            <p>
+                We constructed this website not only to learn more about Django and React programming, but specifically how to 
+                implement such skills in a realistic environment, such as a Website.
+            </p>
+        </div>
+    )
+
+}
+
+export { PageNotFound, About }
