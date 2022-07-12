cdd0df761c2f6dfb037152b0961a8119a3270b22
hanyo2 <hanjien101@gmail.com>
2022-07-12 19:34:42 +0800

    Included dropdown within table

--- pricetracker/frontend/frontend/src/components/table.js
+++ pricetracker/frontend/frontend/src/components/table.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { useApiContext } from './api_fetcher.js';//import these to use api
 import { SearchBar } from './search_bar.js';
-import { DropDown } from './drop_down.js';
+import DropDown from './drop_down';
 
+
+const options = [
+  {
+      id: 1, 
+      value: 'date',
+  },
+  {
+      id: 2, 
+      value: 'price (low-high)',
+  },
+  {
+      id: 2, 
+      value: 'price (high-low)',
+  },
+]
 
 function Table() {
     const fuelprices = useApiContext();//fuel array
@@ -12,6 +27,9 @@ function Table() {
           <h1>Table of all prices recorded this month</h1> 
           <SearchBar />
           <DropDown />
+                  <div className = "container" >
+              <DropDown title  = "Select option" options = {options} />
+              </div>
           <table>
             <tbody>
             <tr>
