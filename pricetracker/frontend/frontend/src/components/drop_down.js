df4bc7c9fc1855ec066eaa080573c89adb719925
kasmello <karmel.gandahusada@gmail.com>
2022-07-14 21:57:44 +0800

    dropdown now fully working

--- pricetracker/frontend/frontend/src/components/drop_down.js
+++ pricetracker/frontend/frontend/src/components/drop_down.js
@@ -1,47 +1,23 @@
 import React, {useState} from 'react'; 
-import { useUpdateContext } from './api_fetcher';
+import { useUpdateContext, useUpdateSearchContext } from './api_fetcher';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
-function DropDown({ title,options = [],multiSelect = false}) 
-{
-    const [open, setOpen] = useState(false); 
-    const [selection, setSelection] = useState([]); 
-    const toggle = () => setOpen (!open); 
+function SearchDropDown() {
+    const [category, setCategory] = useState("Choose category to filter by")
+    const updateSearch = useUpdateSearchContext();
 
-    function handleOnClick(item) {}
-
+    const changeFilter = (filter) => {
+        setCategory(filter);
+        updateSearch(filter.toLowerCase());
+    }
 
     return (
-        
-        <div className = "dd-wrapper">
-            <div 
-                tabIndex={0}
-                className = "dd-header"
-                role = "button" 
-                onKeyPress = {() => toggle (!open)} 
-                onClick = {() =>toggle(!open)} 
-                > 
-                <div className = "dd-header__title">
-                    <p className = "dd-header__title -- bold">{title}</p>
-                </div>
-                {/* <div className = "dd-header__action">
-                    <p>{open ? 'Close' : 'Open'}</p>
-                </div> */}
-            </div>  
-            {open && (
-                <ul className = "dd-list"> 
-                {options.map(item =>(
-                    <list className = "dd-list-item" key={item.id}>
-                        <button type = "button" onClick = {() => handleOnClick(item)}>
-                            <span> {item.value}</span>
-                            <span>Selected...</span>
-                        </button>
-                    </list>
-                ))}
-                </ul>
-            )}
-        </div>
-    )
+      <DropdownButton className='dd' title={ category } variant='secondary'>
+        <Dropdown.Item onClick={() => changeFilter('Brand')}>Brand</Dropdown.Item>
+        <Dropdown.Item onClick={() => changeFilter('Address')}>Address</Dropdown.Item>
+      </DropdownButton>
+    );
+  }
 
-}
-
-export default DropDown
+export default SearchDropDown
