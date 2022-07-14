dab39369a2e1335f601d3a5e568976411dc5650f
kasmello <karmel.gandahusada@gmail.com>
2022-07-15 01:00:10 +0800

    janky context code for date + search term updated

--- pricetracker/frontend/frontend/src/components/api_fetcher.js
+++ pricetracker/frontend/frontend/src/components/api_fetcher.js
@@ -5,6 +5,7 @@ const ApiContext = createContext();
 const UpdateContext = createContext();
 const SearchContext = createContext();
 const UpdateSearchContext = createContext()
+const UpdateDateContext = createContext()
 
 function useApiContext() {
     return useContext(ApiContext)
@@ -22,25 +23,54 @@ function useUpdateSearchContext() {
     return useContext(UpdateSearchContext);
 }
 
+function useUpdateDateContext() {
+    return useContext(UpdateDateContext);
+}
+
 
 function ApiProvider({ children }) {
 
     const [fuelprices, setPrices] = useState([]);
     const [fuelview, setView] = useState([]);
+    const [dateScope, setScope] = useState(1);
+    const [maxScope, setMax] = useState(1);
     const [places, setPlaces] = useState([]);
-    const [searchCat, changeCat] = useState('brand')
+    const [searchCat, changeCat] = useState('brand');
+    const [filterArray, setFilter] = useState([{},{}]);
 
-    const getLink = () => {
+    const getDate = (num) => {
         const date = new Date();
-        const day = date.getDate() -1;
+        const day = date.getDate() - (num);
         const month = date.getMonth() + 1;
         const year = date.getFullYear();
-        console.log(`Fetching from http://127.0.0.1:8000/api/price/from=${year}-${month}-${day}/...`)
-        return `http://127.0.0.1:8000/api/price/from=${year}-${month}-${day}/`
+        return `${year}-${month}-${day}`
+    }
+
+    const getLink = (num) => {
+        const date = getDate(num)
+        console.log(`Fetching from http://127.0.0.1:8000/api/price/from=${date}/...`)
+        return `http://127.0.0.1:8000/api/price/from=${date}/`
     };
 
-    const fetchFuels = () => {
-            fetch(getLink())
+    const changeDateScope = (num) => {
+        setScope(num);        
+        let checkIfReloadNeeded = new Promise((resolve, reject) => {
+            if (num > maxScope) {
+                setMax(num)
+                fetchFuels(num)
+            }
+            resolve()
+        })
+        checkIfReloadNeeded.then(() => changeFilter([1, {
+            cat: 'date',
+            val: getDate(num),
+            exp: '>='
+        }]))
+        
+    }
+
+    const fetchFuels = (num) => {
+            fetch(getLink(num))
             .then(response => response.json()) //converts data
             .then(json => {
                 setPrices(json)
@@ -54,6 +84,7 @@ function ApiProvider({ children }) {
         console.log(`changed category to ${category}`)
     }
 
+
     const fetchPlaces = () => {
         console.log('Fetching from http://127.0.0.1:8000/api/...')
         fetch('http://127.0.0.1:8000/api/')
@@ -63,79 +94,66 @@ function ApiProvider({ children }) {
         })
     }
 
-    const filterData = (filterArray) => {
+    const changeFilter = (filt) => {
+        var tempArray = filterArray;
+        tempArray[filt[0]] = filt[1]
+        setFilter(tempArray)
+        filterData(tempArray)
+    }
+    const filterData = (array) => {
     //example filter data
     //row.cat = "brand", row.val = "ampol", row.exp = "greater"
-        filterArray.map((rowFilt) => {       
-            if (rowFilt.val.length > 0) {
-                const filterFunction = (row) => {
-                    const category = {
-                        brand: row.brand,
-                        date: row.date,
-                        price: row.price,
-                        address: row.address
-                    }
-                    if (rowFilt.exp === '>=') {
-                        return category[rowFilt.cat] >= rowFilt.val;
-            
-                    }
-                    else if (rowFilt.exp === '=') {
+        let tempArray = fuelprices
+        array.map((rowFilt) => {   
+            if (Object.keys(rowFilt).length > 0) {   
+                if (rowFilt.cat === 'brand' || rowFilt.cat === 'address') {
+                    const filterFunction = (row) => {
+                        const category = {
+                            brand: row.brand,
+                            address: row.address,
+                        }
                         return category[rowFilt.cat].toLowerCase().includes(rowFilt.val.toLowerCase());
-            
-                    }
-                    else if (rowFilt.exp === '<=') {
-                        return category[rowFilt] <= rowFilt.val;
-                    }
-                }
-                setView(fuelprices.filter(filterFunction))
                 
-            }
-            else {
-                setView(fuelprices)
-            }
+                    }
+                    tempArray = tempArray.filter(filterFunction)
+                    
+                } else if (rowFilt.cat==='date'){
+                    const filterFunction = (row) => {
+
+                        return Date.parse(row.date) >= Date.parse(rowFilt.val);
+                
+                    }
+                    tempArray = tempArray.filter(filterFunction)
+
+                };
+            };
             
         });
+        setView(tempArray)
         
     };
 
     useEffect(() => {
-        fetchFuels();
-        fetchPlaces();
+        fetchFuels(1);
     },[]); //acts as component did mount, only executes once on launch
 
     
     return (
         <ApiContext.Provider value = { fuelview }>
-        <UpdateContext.Provider value = { filterData }>
+        <UpdateContext.Provider value = { changeFilter }>
         <SearchContext.Provider value = { searchCat }>
         <UpdateSearchContext.Provider value = {changeSearchCat}>
+        <UpdateDateContext.Provider value = { changeDateScope }>
             { children }
+        </UpdateDateContext.Provider>
         </UpdateSearchContext.Provider>
         </SearchContext.Provider>
         </UpdateContext.Provider>
         </ApiContext.Provider>
     );
 
-    
-    // return (
-    // <div className='Infodisplay'>
-    //     <h1>All prices recorded this month</h1> 
-    //     {fuelprices.map((price) => (
-    //         //ol is ordered list
-    //         <ol key = { price.id} > 
-    //         Brand: { price.brand },
-    //         Date: { price.date },
-    //         Price: { price.price },
-    //         Address: { price.address }
-
-    //         </ol>
-    //     ))
-    //     }
-    // </div>
-    // );
-
-
 }
 
 
-  export { ApiProvider, useApiContext, useUpdateContext, useSearchContext, useUpdateSearchContext }
+  export { ApiProvider, useApiContext, useUpdateContext, useSearchContext, useUpdateSearchContext,
+             useUpdateDateContext }
