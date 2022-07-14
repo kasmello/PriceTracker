24170c179b2e91249641e23c3349c83c7ad408db
kasmello <karmel.gandahusada@gmail.com>
2022-07-14 19:51:37 +0800

    separated table into own module

--- pricetracker/frontend/frontend/src/components/table.js
+++ pricetracker/frontend/frontend/src/components/table.js
@@ -1,37 +1,12 @@
 import React from 'react';
 import { useApiContext } from './api_fetcher.js';//import these to use api
-import { SearchBar } from './search_bar.js';
-import DropDown from './drop_down';
-import Chart from './timeseries';
-
-
-const options = [
-  {
-      id: 1, 
-      value: 'date',
-  },
-  {
-      id: 2, 
-      value: 'price (low-high)',
-  },
-  {
-      id: 2, 
-      value: 'price (high-low)',
-  },
-]
 
 function Table() {
     const fuelprices = useApiContext();//fuel array
-    // const filterFuels = useUpdateContext();
     return (
-        <div className="Table">
-          <h1>Table of all prices recorded this month</h1> 
-          <SearchBar />
-          <div className = "container" >
-            <DropDown title  = "Select option" options = {options} />
-          </div>
-          <table>
-            <tbody>
+      <div className="TableContainer">
+        <table>
+          <tbody>
             <tr>
               <th>Brand</th>
               <th>Date</th>
@@ -49,18 +24,12 @@ function Table() {
                 </tr>
               )
             })}
-            </tbody>
-          </table>
-          <button>GraphView</button>
-          <Chart />
-        </div>
-      );
+          </tbody>
+        </table>
+      
+      </div>
+    );
+  
+  }
 
-     
-    }
-
-
-
-
-
-  export default Table
+export default Table;
