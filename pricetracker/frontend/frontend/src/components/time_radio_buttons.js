67622c3eef48676240d1d394f8c0c71f527f18a5
kasmello <karmel.gandahusada@gmail.com>
2022-07-15 01:01:23 +0800

    radio buttons for time added

---
+++ pricetracker/frontend/frontend/src/components/time_radio_buttons.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { useUpdateDateContext } from './api_fetcher';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
+function ChooseMultiTime() {
+    const updateSearch = useUpdateDateContext();
+    const changeFilter = (filter) => {
+        updateSearch(parseInt(filter));
+    }
+
+    return (
+        <FormControl className='timeRadio'>
+            <FormLabel id="demo-row-radio-buttons-group-label">Time span of data</FormLabel>
+            <RadioGroup
+                row
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                defaultValue="1"
+                name="row-radio-buttons-group"
+                onChange={(e,value) => changeFilter(value)}
+            >
+                <FormControlLabel value="1" control={<Radio />} label="Day" />
+                <FormControlLabel value="7" control={<Radio />} label="Week" />
+                <FormControlLabel value="30" control={<Radio />} label="Month" />
+            </RadioGroup>
+        </FormControl>
+    )
+}
+
+export default ChooseMultiTime
