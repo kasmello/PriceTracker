2ae68126a1fbf4ab18a7d5eb55a2728a9c6b7d85
hanyo2 <hanjien101@gmail.com>
2022-07-18 23:54:53 +0800

    Added data table

---
+++ pricetracker/frontend/frontend/src/components/data_table.js
@@ -0,0 +1,61 @@
+import React from "react"
+import MUIDataTable from "mui-datatables";
+import { useApiContext } from './api_fetcher.js';//import these to use api
+
+
+const columns = [
+ {
+  name: "brand",
+  label: "Brand",
+  options: {
+   filter: false,
+   sort: true,
+  }
+ },
+ {
+  name: "date",
+  label: "Date",
+  options: {
+   filter: false,
+   sort: true,
+  }
+ },
+ {
+  name: "price",
+  label: "Price",
+  options: {
+   filter: false,
+   sort: true,
+  }
+ },
+ {
+  name: "address",
+  label: "Address",
+  options: {
+   filter: false,
+   sort: true,
+  }
+ },
+];
+
+const options = {
+  selectableRows: false, 
+  download: false,
+  search: false,
+  print: false, 
+  viewColumns: false,
+  filter: false
+};
+
+function DataTable() {
+  const fuelprices = useApiContext();//fuel array
+  return(<MUIDataTable
+  data={fuelprices}
+  columns={columns}
+  options = {options} />)
+}
+
+export default DataTable
+
+
+
