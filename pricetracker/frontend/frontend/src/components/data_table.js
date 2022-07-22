a12a165a10f34122985f8a4d19c92e095a4d499d
kasmello <karmel.gandahusada@gmail.com>
2022-07-22 12:54:56 +0800

    added context givers

--- pricetracker/frontend/frontend/src/components/data_table.js
+++ pricetracker/frontend/frontend/src/components/data_table.js
@@ -1,6 +1,31 @@
-import React from "react"
+import React, { createContext, useContext, useState } from "react"
 import MUIDataTable from "mui-datatables";
 import { useApiContext } from './api_fetcher.js';//import these to use api
+
+const EnableSelect = createContext()
+const SelectContext = createContext()
+
+function useEnableSelect() {
+  return useContext(EnableSelect)
+}
+
+function useSelect() {
+  return useContext(SelectContext)
+}
+
+function SelectedChanger({ children }) {
+  const [selected, selectedEnable] = useState('none')
+  const changeSelected = () => {
+    selectedEnable(selected == 'none' ? 'multiple' : 'none')
+  }
+  return (
+    <SelectContext.Provider value = { selected }>
+    <EnableSelect.Provider value = { changeSelected }>
+      { children }
+    </EnableSelect.Provider>
+    </SelectContext.Provider>
+  )
+}
 
 
 const columns = [
@@ -38,24 +63,27 @@ const columns = [
  },
 ];
 
-const options = {
-  selectableRows: false, 
-  download: false,
-  search: false,
-  print: false, 
-  viewColumns: false,
-  filter: false
-};
 
 function DataTable() {
   const fuelprices = useApiContext();//fuel array
+  const selected = useSelect();
+  const options = {
+    selectableRows: selected, 
+    download: false,
+    search: false,
+    print: false, 
+    viewColumns: false,
+    filter: false,
+    customToolbarSelect: () => {}
+  };
+  
   return(<MUIDataTable
   data={fuelprices}
   columns={columns}
   options = {options} />)
 }
 
-export default DataTable
+export { DataTable, useEnableSelect, SelectedChanger }
 
 
 
