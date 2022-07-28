51b7e6edfa9bf979498a27c5f5dda6fc28dd6955
kasmello <karmel.gandahusada@gmail.com>
2022-07-28 17:47:09 +0800

    made date scope much simpler and avoids rerenders

--- pricetracker/frontend/frontend/src/components/api_fetcher.js
+++ pricetracker/frontend/frontend/src/components/api_fetcher.js
@@ -12,9 +12,11 @@ function editDateScope() {
     return useContext(ChangeDateScope)
 }
 
+
 function ApiProvider({ children }) {
 
     const [fuelprices, setPrices] = useState([]);
+    const [maxFilter, setMax] = useState(1)
 
     const getDate = (num) => {
         const date = new Date();
@@ -24,34 +26,21 @@ function ApiProvider({ children }) {
         const year = date.getFullYear();
         return `${year}-${month}-${day}`
     }
-
     const getLink = (num) => {
         const date = getDate(num)
         console.log(`Fetching from http://127.0.0.1:8000/api/price/from=${date}/...`)
         return `http://127.0.0.1:8000/api/price/from=${date}/`
     };
 
-    const changeDateScope = (num) => {  
-        const grabEarliestDate = () => {
-            fuelprices.sort((a, b) => {
-                let da = new Date(a.date).setHours(0, 0, 0, 0), //rounds to nearest date
-                    db = new Date(b.date).setHours(0, 0, 0, 0);
-                return da - db;  
-            })  
-            return fuelprices[0].date
-        };
-        
-        const checkIfReloadNeeded = ([old_date, new_date]) => {
-            let oldd = new Date(old_date).setHours(0, 0, 0, 0),
-                newd = new Date(new_date).setHours(0, 0, 0, 0)
-            if (newd < oldd) {
-                console.log('Client does not have data for ' + new_date + ', fetching...')
-                fetchFuels(num)
-            }
+    const changeDateScope = (num) => {          
+        const checkIfReloadNeeded = (old_num, new_num) => {
+            if (parseInt(new_num) > parseInt(old_num)) {
+                console.log('Client does not have data for ' + new_num + ' days ago, fetching...')
+                fetchFuels(new_num)
+                setMax(new_num)
+            } 
         }
-        var new_date = getDate(num)
-        const earliest_date = grabEarliestDate()
-        checkIfReloadNeeded([earliest_date, new_date])
+        checkIfReloadNeeded(maxFilter, num)
     }
 
     const fetchFuels = (num) => {
