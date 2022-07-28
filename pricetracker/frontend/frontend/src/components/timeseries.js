bf962c40cab59637e8a7a1cfaf761bb9caa530aa
kasmello <karmel.gandahusada@gmail.com>
2022-07-28 17:46:26 +0800

    in progress

--- pricetracker/frontend/frontend/src/components/timeseries.js
+++ pricetracker/frontend/frontend/src/components/timeseries.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactFC from "react-fusioncharts";
 import FusionCharts from "fusioncharts";
 import MSLine from "fusioncharts/fusioncharts.charts";
 import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
-import { getFilteredData } from './filter';
+import { getFilteredData, useFuelSelect } from './filter';
+import { useApiContext, editDateScope } from './api_fetcher.js'
 
 ReactFC.fcRoot(FusionCharts, MSLine, FusionTheme);
 
@@ -26,22 +27,79 @@ const incrementDate = (date) => {
 }
 
 const getAllDatesTilCurr = (date, curr) => {
-    let temp_arr = {"category":[]};
+    let x = {category:[]};
     while (Date.parse(date) <= Date.parse(curr)) {
-        let temp_object = {'label': date}
-        temp_arr.category.push(temp_object)
+        let temp_object = {label: date}
+        x.category.push(temp_object)
         date = incrementDate(date)
-
     }
-    return temp_arr
+    return x
 }
 
+
+
+const convertToTimeSeries = (address, x, allData) => {
+    const filterByAddress = (row) => {
+        return address==row.address
+    }
+    var timeseriesData = allData.filter(filterByAddress)
+    console.log(allData.filter(filterByAddress))
+    timeseriesData.sort((a, b) => {
+        let da = new Date(a.date).setHours(0, 0, 0, 0), //rounds to nearest date
+            db = new Date(b.date).setHours(0, 0, 0, 0);
+        return db - da;  
+    }) 
+    var j = 0
+    var oldPrice = 0
+    var y = []
+    
+    timeseriesData.forEach(row => {
+        let match = false
+        while (!match) {
+            if (Date.parse(row.date)===Date.parse(x.category[j].label)) {      
+                oldPrice = row.price
+                match = true
+            } 
+            y.push({value: oldPrice})
+            j++;
+            console.log(row.date + x.category[j].label)
+            if (j >= x.category.length) {match = true}
+        }
+
+    })
+
+}
+
+const getAllAddresses = () => {
+    const selectedIndex = useFuelSelect();
+    const filterfunc = getFilteredData()
+    const filteredData = filterfunc()
+    const allSelectedAddresses = selectedIndex.map(num => {
+        return filteredData[num].address
+    })
+    return allSelectedAddresses
+};
 
 
 
 function Chart() {
     const linedata = [];//fuel data
-
+    const changeDateScope = editDateScope();
+    var allData = []
+    
+    useEffect(() => {
+        const changeScopeToThirtyDays = async () => {
+            allData = await changeDateScope(30)
+        }
+        changeScopeToThirtyDays().catch(console.error)
+    },[]);
+    
+    const thirty_days_ago = getDate(30), today = getDate(0)
+    const x = getAllDatesTilCurr(thirty_days_ago, today)
+    const addresses = getAllAddresses()
+    const timeSeriesData = addresses.map(address => {
+        convertToTimeSeries(address, x, allData)
+    })
     const chartConfigs =  {
         type: "msline", // The chart type
         width: "700", // Width of the chart
