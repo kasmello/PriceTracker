83b619bf123f7fcaa230de1602355d292651d582
kasmello <karmel.gandahusada@gmail.com>
2024-01-15 20:15:52 +0800

    Updates

---
+++ precache-manifest.2364d88d458adaed19a9afac4ad75cc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "944bb1c9bd7bf909a53f",
+    "url": "/PriceTracker/static/css/main.cdf2aa8e.chunk.css"
+  },
+  {
+    "revision": "944bb1c9bd7bf909a53f",
+    "url": "/PriceTracker/static/js/main.b5809377.chunk.js"
+  },
+  {
+    "revision": "f029db70331242079e19",
+    "url": "/PriceTracker/static/js/runtime~main.629b4b6e.js"
+  },
+  {
+    "revision": "d67a6a752ae153b64cf1",
+    "url": "/PriceTracker/static/js/2.dfc6966d.chunk.js"
+  },
+  {
+    "revision": "7dce0f2c43bc3bc323e4",
+    "url": "/PriceTracker/static/js/3.019e24d2.chunk.js"
+  },
+  {
+    "revision": "529e51c04dfdd46f43db795cd8f2f4f0",
+    "url": "/PriceTracker/index.html"
+  }
+];
